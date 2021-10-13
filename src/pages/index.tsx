ac6a16b579e0ca5cff6e0ecbf372f47f72683a16
Jardel Urban <jardelurban3@gmail.com>
2021-10-13 18:05:27 -0300

    Alterando getServerSideProps para getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stipe';
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JkDpsERL8L5GIWyKm6X1Avu')
 
   const product = {
@@ -48,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
      props: {
        product
-     }
+     },
+     revalidate: 60 * 60 * 24 // 24 hours
   }
 }
